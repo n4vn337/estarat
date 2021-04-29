34dbe31fd5c30c188b60a0f2b38ff5ccf2855ddc
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2021-04-29 17:58:10 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -6,7 +6,6 @@
 
 $IP=$1
 
-cp all.txt $IP
 mkdir -p $IP/check_me $IP/origin
 
 
@@ -15,7 +14,10 @@ mkdir -p $IP/check_me $IP/origin
 
 amass enum -d $IP > origin/amass.txt
 subfinder -d $IP -t 25 -timeout 5 -silent > origin/subfinder.txt
-echo origin/amass.txt> origin/origin_subs.txt; echo origin/subfinder.txt> origin/origin_subs.txt
+
+altdns -i check_me/subs.txt -o data_output -w ~/tools/words.txt -r -s origin/altdns.txt
+
+echo origin/amass.txt> origin/origin_subs.txt; echo origin/subfinder.txt> origin/origin_subs.txt; echo origin/altdns.txt> origin/origin_subs.txt
 uniq origin/origin_subs.txt> origin/hosts.txt
 cat origin/hosts.txt | httpx -silent> origin/subs.txt
 cp origin/subs.txt check_me/subs.txt
@@ -26,14 +28,20 @@ cp origin/subs.txt check_me/subs.txt
 # screenshot
 # ----------
 
-cat check_me/subs.txt | aquatone -chrome-path /root/hacking/chromium-latest-linux/latest/chrome
+cat check_me/subs.txt | aquatone -chrome-path ~/tools/latest/chrome -out aquatone
+
+
+# vulns
+# ------------------
+
+subjack -w check_me/subs.txt -t 100 -timeout 30 -o origin/subjack.txt -ssl
+# cat origin/subjack.txt | 
+
+nuclei -l chec_me/subs.txt -t ~/tools/nuclei-templates/*/
 
 
 # service enum/ port scan
 # -----------------------
-
-#masscan -p1-65535 $(dig +short $IP) --rate 10000
-#nmap -sV -p <ports from masscan> $IP --min-rate=10000
 
 # while read -r line; do masscan -p1-65535 $(dig +short $line) --rate 10000; done < subs.txt|tee ports
 # while read -r line; do nmap -sV -p <ports from masscan> $IP --min-rate=10000; done < subs.txt
@@ -45,8 +53,8 @@ cat check_me/subs.txt | naabu -nmap-cli 'nmap -sV -oX check_me/naabu-output'
 # --------------
 
 # linkfinder -i "https://www.$IP/" -d -o cli
-while read -r line; do python linkfinder.py -i "https://$line" -d -o cli; done<~/Desktop/subs.txt | tee origin/links.txt
+while read -r line; do python linkfinder.py -i "https://$line" -d -o cli; done< check_me/subs.txt | tee origin/linkfinder.txt
 
+cp origin/linkfinder.txt check_me/linkfinder.txt
 # echo "www.$IP" | otxurls
 # echo "www.$IP" | waybackurls
-
