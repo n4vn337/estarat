a3f27fe3ca986ee10bcea3421ee7cfa4e5a2cc3f
Navneet Anand <59524376+n4vn337@users.noreply.github.com>
2021-04-29 18:02:11 +0000

    Update script.sh

--- script.sh
+++ script.sh
@@ -20,7 +20,6 @@ altdns -i check_me/subs.txt -o data_output -w ~/tools/words.txt -r -s origin/alt
 echo origin/amass.txt> origin/origin_subs.txt; echo origin/subfinder.txt> origin/origin_subs.txt; echo origin/altdns.txt> origin/origin_subs.txt
 uniq origin/origin_subs.txt> origin/hosts.txt
 cat origin/hosts.txt | httpx -silent> origin/subs.txt
-cp origin/subs.txt check_me/subs.txt
 #sed -r 's/$/.$IP/' -i all.txt
 #massdns -r lists/resolvers.txt -t CNAME all.txt -o s > results
 
@@ -46,7 +45,7 @@ nuclei -l chec_me/subs.txt -t ~/tools/nuclei-templates/*/
 # while read -r line; do masscan -p1-65535 $(dig +short $line) --rate 10000; done < subs.txt|tee ports
 # while read -r line; do nmap -sV -p <ports from masscan> $IP --min-rate=10000; done < subs.txt
 
-cat check_me/subs.txt | naabu -nmap-cli 'nmap -sV -oX check_me/naabu-output'
+cat check_me/subs.txt | naabu -nmap-cli 'nmap -sV -oX origin/naabu.txt'
 
 
 # links to check
@@ -55,6 +54,14 @@ cat check_me/subs.txt | naabu -nmap-cli 'nmap -sV -oX check_me/naabu-output'
 # linkfinder -i "https://www.$IP/" -d -o cli
 while read -r line; do python linkfinder.py -i "https://$line" -d -o cli; done< check_me/subs.txt | tee origin/linkfinder.txt
 
-cp origin/linkfinder.txt check_me/linkfinder.txt
 # echo "www.$IP" | otxurls
 # echo "www.$IP" | waybackurls
+
+
+copying files to check_me
+-------------------------
+
+cp origin/subs.txt check_me/subs.txt
+cp origin/subjack.txt check_me/origin/subjack.txt
+cp origin/naabu.txt check_me/naabu.txt
+cp origin/linkfinder.txt check_me/linkfinder.txt
